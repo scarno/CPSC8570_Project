14cd6e789caee1f19312118c7f5351eaa35159b0
ne0nV <117318940+ne0nV@users.noreply.github.com>
2025-04-16 15:51:53 -0400

    Update label_flipping.py
    Fix initial commit

--- attacks/label_flipping.py
+++ attacks/label_flipping.py
@@ -1,1 +1,17 @@
-<full label_flipping.py content from earlier>
+"""
+label_flipping.py
+
+Implements a simple label-flipping attack.
+"""
+
+def poison_update(update):
+    """
+    Simulates a label-flipping attack by inverting the sign of the update.
+    
+    Parameters:
+      update (np.array): Original update.
+      
+    Returns:
+      np.array: Poisoned update.
+    """
+    return -update
